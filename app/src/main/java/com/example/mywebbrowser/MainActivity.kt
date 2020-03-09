1645b3e06935e79b0434fe10bca12e74df444f06
ndr28s <ndr28s@nate.com>
2020-03-09 14:38:26 +0900

    init

---
+++ app/src/main/java/com/example/mywebbrowser/MainActivity.kt
@@ -0,0 +1,124 @@
+package com.example.mywebbrowser
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView.apply{
+            settings.javaScriptEnabled = true
+            webViewClient = WebViewClient()
+        }
+
+        webView.loadUrl("http://www.google.com")
+
+        urlEditText.setOnEditorActionListener { _, actionId, _ ->
+            if(actionId == EditorInfo.IME_ACTION_SEARCH) {
+                webView.loadUrl(urlEditText.text.toString())
+                true
+            }else{
+                false
+            }
+        }
+
+        registerForContextMenu(webView)
+    }
+
+
+    override fun onBackPressed(){
+        if(webView.canGoBack()) {
+            webView.goBack()
+        }else{
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.action_google, R.id.action_home -> {
+                webView.loadUrl("http://www.google.com")
+                return true
+            }
+            R.id.action_naver -> {
+                webView.loadUrl("http://naver.com")
+                return true
+            }
+            R.id.action_daum -> {
+                webView.loadUrl("http://daum.net")
+                return true
+            }
+            R.id.action_call -> {
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel:010-9193-1947")
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }
+                return true
+            }
+            R.id.action_send_text -> {
+                val uri = Uri.parse("smsto:01091931947")
+                val intent = Intent(Intent.ACTION_SENDTO, uri)
+                startActivity(intent)
+                return true
+            }
+            R.id.action_email -> {
+                val intent = Intent(Intent.ACTION_SENDTO)
+                intent.data = Uri.parse("mailto:") // only email apps should handle this
+                intent.putExtra(Intent.EXTRA_EMAIL, "bygwna@gmail.com")
+                intent.putExtra(Intent.EXTRA_SUBJECT,"Feedback")
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }
+
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.context, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        val webUrl = webView.url
+        when (item?.itemId) {
+            R.id.action_share -> {
+                val sendIntent: Intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, webUrl)
+                    type = "text/plain"
+                }
+                val shareIntent = Intent.createChooser(sendIntent, null)
+                startActivity(shareIntent)
+
+                return true
+            }
+            R.id.action_browser -> {
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(webUrl))
+                startActivity(browserIntent)
+                return true
+            }
+        }
+        return super.onContextItemSelected(item)
+    }
+}
